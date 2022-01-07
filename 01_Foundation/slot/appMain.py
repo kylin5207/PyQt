1ac28bce085b3c010fb6da3c231af6e654b9c112
Kylin <38106679+kylin5207@users.noreply.github.com>
2022-01-07 15:42:05 +0800

    Update appMain.py

--- 01_Foundation/slot/appMain.py
+++ 01_Foundation/slot/appMain.py
@@ -6,8 +6,8 @@ appMain.py 的功能是创建应用程序和主窗体，然后显示主窗体，
 它将 myDialog.py 文件的测试运行部分单独拿出来作为一个文件。
 当一个应用程序有多个窗体，并且窗体之间有数据传递时，appMain.py 负责创建应用程序的主窗体并运行起来，这样使整个应用程序的结构更清晰。
 
-:Author: Shangmengqi@tsingj.com
-:Last Modified by: Shangmengqi@tsingj.com
+:Author: Kylin
+:Last Modified by: kylin.smq@qq.com
 """
 import sys
 from PyQt5.QtWidgets import QApplication
